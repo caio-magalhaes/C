56b962212c9422272940ba5e65efd2489d3722ac
Caio <caiocostauni@outlook.com>
2025-05-22 22:31:57 -0300

    Struct

---
+++ struct/struct01.c
@@ -0,0 +1,5 @@
+struct MyStructure {   // Structure declaration
+  int myNum;           // Member (int variable)
+  char myLetter;       // Member (char variable)
+}; // End the structure with a semicolon
+
